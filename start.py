69e31b98f7b4282d462a36bfa61b7c6e3db7c60d
Md.Mahir Faisal <121873793+mahirfaisal13@users.noreply.github.com>
2023-01-04 20:10:24 +0600

    Update start.py

--- start.py
+++ start.py
@@ -13,3 +13,12 @@ a = "Python"
 b = "is"
 c = "awesome"
 print(a, b, c)
+
+
+
+numbers = [3, 6, 2, 10, 8, 4, 14, 2]
+max = numbers [0]
+for number in numbers:
+    if number > max:
+        max = number
+        print(max)
